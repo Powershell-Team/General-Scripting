6c8c72b8f378a98ab129edd7981078b6d87f43f2
ariekreb <46277658+ariekreb@users.noreply.github.com>
2019-01-10 11:08:57 +0200

    Installation Script

---
+++ Get-ADGroupReport-Install.ps1
@@ -0,0 +1,6 @@
+﻿$url = "https://raw.githubusercontent.com/Powershell-Team/cmdlets/master/Get-ADGroupReport"
+mkdir "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Get-ADGroupReport"
+$path = "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Get-ADGroupReport\Get-ADGroupReport.psm1"
+Start-BitsTransfer -Source $url -Destination $path
+Import-Module "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Get-ADGroupReport\Get-ADGroupReport.psm1"
+Write-Host "You Can Now Use Get-ADGroupReport" -ForegroundColor Cyan
